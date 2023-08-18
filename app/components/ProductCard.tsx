faa7bdda9746adea51a6048a3d3cc5356a6e68d7
Sebastianmoldrup <sebastian.moldrup@gmail.com>
2023-08-18 12:52:26 +0200

    design changes

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
+import { IconCart, IconSale } from './Icons';
 
 export function ProductCard() {
   const [products, setProducts] = useState([]);
@@ -16,7 +17,7 @@ export function ProductCard() {
   }, []);
 
   return (
-    <ul className='flex flex-wrap gap-10 my-10 mx-auto row-start-1 col-start-1'>
+    <ul className='flex flex-wrap justify-center gap-10 my-10 mx-auto row-start-1 col-start-1'>
       {products.map(
         (product: {
           id: number;
@@ -35,18 +36,30 @@ export function ProductCard() {
           return (
             <li
               key={product.id}
-              className='flex flex-col items-center justify-between w-[200px] h-[150px] shadow-xl'
+              className='flex flex-col flex-wrap relative items-center justify-between group w-[200px] h-[180px] shadow-xl text-black bg-white rounded-lg group-hover:brightness-75'
             >
-              <div className='flex justify-between w-full'>
-                <div>{product.price}</div>
-                <div className='relative w-[50px] h-[50px]'>
-                  <Image src={product.thumbnail} alt='' layout='fill' />
-                </div>
+              <div className='flex items-center justify-start w-full pt-4 px-4'>
+                <span className='line-clamp-1 text-center font-bold text-lg'>
+                  {product.title}
+                </span>
               </div>
-              <div className='flex flex-wrap justify-between w-full'>
-                <span>{product.title}</span>
-                <span>{product.category}</span>
+              <div className='relative w-[80px] h-[80px]'>
+                <Image
+                  src={product.thumbnail}
+                  alt=''
+                  layout='fill'
+                  className='rounded-md object-contain'
+                />
               </div>
+              <div className='flex justify-between px-4 pb-2 w-full items-center'>
+                <span>{product.price + '$'}</span>
+                <span>
+                  <IconCart />{' '}
+                </span>
+              </div>
+              <a className='hover-button absolute cursor-pointer top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-green-400 brightness-100 text-white px-4 py-2 rounded-md hidden group-hover:block'>
+                Buy
+              </a>
             </li>
           );
         }
--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,7 @@ import { Divider } from './components/Divider';
 export default function Home() {
   return (
     <main className=''>
-      <section className='grid'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 1000 500'
-          width='100%'
-          height='100%'
-          className='row-start-1 col-start-1'
-        >
-          <rect width='100%' height='100%' fill='#3498db' />
-
-          <path
-            fill='#ffffff'
-            d='M0 300 Q250 400, 500 300 Q750 200, 1000 300 V500 H0 Z'
-          />
-        </svg>
+      <section className=''>
         <ProductCard />
       </section>
     </main>
