95bc962256f55e967e8d5e57f7de715a231d950a
Sebastianmoldrup <sebastian.moldrup@gmail.com>
2023-09-04 14:53:39 +0200

    wip

--- app/components/BestSeller.tsx
+++ app/components/BestSeller.tsx
@@ -1,22 +1,3 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { IconCart, IconSale } from './Icons';
-
-// export function BestSeller() {
-//   const [products, setProducts] = useState([]);
-//   const randomProducts: [] = [];
-
-//   useEffect(() => {
-//     async function getProducts() {
-//       const response = await fetch('/api/products');
-//       const data = await response.json();
-//       setProducts(data.products);
-//       console.log(data.thumbnail);
-//     }
-
-//     getProducts();
-//     console.log(products);
-//   }, []);
-
-//   return <div>{products.map((product) => {})}</div>;
-// }
