ac9c13df65af64fd2961464c5643bc8ed83be0d1
Sebastianmoldrup <sebastian.moldrup@gmail.com>
2023-09-04 14:52:08 +0200

    disabled bestseller to deploy early

--- app/components/BestSeller.tsx
+++ app/components/BestSeller.tsx
@@ -2,21 +2,21 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { IconCart, IconSale } from './Icons';
 
-export function BestSeller() {
-  const [products, setProducts] = useState([]);
-  const randomProducts: [] = [];
+// export function BestSeller() {
+//   const [products, setProducts] = useState([]);
+//   const randomProducts: [] = [];
 
-  useEffect(() => {
-    async function getProducts() {
-      const response = await fetch('/api/products');
-      const data = await response.json();
-      setProducts(data.products);
-      console.log(data.thumbnail);
-    }
+//   useEffect(() => {
+//     async function getProducts() {
+//       const response = await fetch('/api/products');
+//       const data = await response.json();
+//       setProducts(data.products);
+//       console.log(data.thumbnail);
+//     }
 
-    getProducts();
-    console.log(products);
-  }, []);
+//     getProducts();
+//     console.log(products);
+//   }, []);
 
-  return <div>{products.map((product) => {})}</div>;
-}
+//   return <div>{products.map((product) => {})}</div>;
+// }
