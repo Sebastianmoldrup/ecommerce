1be353b82f6dc47e81a06ccf2efadc840d9e38a8
Sebastianmoldrup <sebastian.moldrup@gmail.com>
2023-09-04 14:56:20 +0200

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 'use client';
 import Image from 'next/image';
 import { ProductOverview } from './components/ProductOverview';
-import { BestSeller } from './components/BestSeller';
 
 export default function Home() {
   return (
     <main className='flex h-screen w-full items-center justify-center'>
-      <BestSeller />
       <div className='flex h-[320px] w-[320px] items-center justify-center rounded-md bg-main-100 shadow-2xl'>
         <h2 className='font-bold'>Welcome to WedgeWood</h2>
       </div>
