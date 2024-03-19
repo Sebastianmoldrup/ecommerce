d389c9ac6664fbfc8b0c14a7414d76ae26217b24
Sebastianmoldrup <sebastian.moldrup@gmail.com>
2024-03-19 13:59:39 +0100

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import Shop from './shop/page';
 import CarouselShowcase from './_components/carousel';
+import Hero from './_components/hero';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <CarouselShowcase />
+      <Hero />
     </main>
   );
 }
